fcc7dc753f05cf16867bc16413c271a2ebbe5ee0
diaoerchan <gzygzy.100@163.com>
2022-04-15 11:16:01 +0800

    fix bug

--- models/answer.go
+++ models/answer.go
@@ -3,11 +3,11 @@ package models
 type Answer struct {
 	UserID    int    `gorm:"column:user_id"`
 	Email     string `json:"email"`
-	Question1 int    `gorm:"question1id; not null"`
+	Question1 int    `gorm:"column:question1id"`
 	Answer1   string `json:"answer1"`
-	Question2 int    `gorm:"question1id; not null"`
+	Question2 int    `gorm:"column:question2id"`
 	Answer2   string `json:"answer2"`
-	Question3 int    `gorm:"question1id; not null"`
+	Question3 int    `gorm:"column:question3id"`
 	Answer3   string `json:"answer3"`
 }
 
--- repository/authorByZone.go
+++ repository/authorByZone.go
@@ -8,7 +8,7 @@ import (
 
 func GetAuthorRankingByZone(zone string, num int) (*[]models.Author, error) {
 	result := []models.Author{}
-	err := models.DbClient.MsClient.Debug().Model(&models.Game{}).
+	err := models.DbClient.MsClient.Model(&models.Game{}).
 		Select("url", "person.nickname as nickname", "sum(games.like_num) as like_num").
 		Joins("left join person on games.uid = person.id").
 		Joins("join person_imgs on games.uid = person_imgs.uid").Where("zone=?", zone).
